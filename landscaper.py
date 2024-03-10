b4caa975dd01fef38183c6b0e321ec5746d72529
Alex Greenberg <acgreenberg2@gmail.com>
2024-03-10 15:28:54 -0400

    able to update the cut_grass function per tool

--- landscaper.py
+++ landscaper.py
@@ -2,8 +2,7 @@
 
 player_status = {
     'money': 0,
-    'tool': 0,
-    'chargeable': 0
+    'tool': 0
 }
 
 ## tool List
@@ -18,20 +17,40 @@ tool_list = [
 
 ## a player can give input to cut grass, upgrade a tool, or quit the game
 
-def get_input():
-    player_input = input("do you want to cut grass, upgrade a tool, or quit the game?")
-    if(player_input == "cut"):
-        print("player wants to cut")
-    elif(player_input == "upgrade"):
-        print("player wants to upgrade")
-    elif(player_input == "quit"):
-        print("player wants to quit")
-    else:
-        print("player needs to type cut, upgrade or quit to proceed!")
+# def get_input():
+#     player_input = input("do you want to cut grass, upgrade a tool, or quit the game?")
+#     if(player_input == "cut"):
+#         print("player wants to cut")
+#     elif(player_input == "upgrade"):
+#         print("player wants to upgrade")
+#     elif(player_input == "quit"):
+#         print("player wants to quit")
+#     else:
+#         print("player needs to type cut, upgrade or quit to proceed!")
 
-get_input()
+# get_input()
 
-## a player can cut the grass and make
+## a player can cut the grass and make money dependent on the tool they have. the player_status should update their money only in this case
+
+def cut_grass():
+    current_tool_ind = player_status['tool']
+    print(current_tool_ind)
+    tool_now = tool_list[current_tool_ind]
+    print(tool_now)
+    service_charge = tool_now['chargeable']
+    print(service_charge)
+    player_status['money'] += service_charge
+    return player_status
+
+print(cut_grass())
+
+print(cut_grass())
+print(cut_grass())
+print(cut_grass())
+
+
+
+
 
 
     
